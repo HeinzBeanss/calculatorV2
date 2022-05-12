0bc4b7ff16a9c5e9f9b07df5a0f13ed47400c75b
David Bean <heinzbeansss@gmail.com>
2022-05-12 22:50:52 +0100

    Semi functioning calculator

--- calculatorV2.js
+++ calculatorV2.js
@@ -1,5 +1,85 @@
+function add(num1, num2) { 
+    display.textContent = (num1 + num2);
+};
+
+function subtract(num1, num2) { 
+    display.textContent = (num1 - num2);
+};
+
+function multiply(num1, num2) { 
+    display.textContent = (num1 * num2);
+};
+
+function divide(num1, num2) { 
+    display.textContent = (num1 / num2);
+};
+
+function operate() {
+
+    num2 = Number(display.textContent);
+    console.log(num1);
+    console.log(num2);
+
+    if (operatorSelected == "add") {
+        add(num1, num2)
+    } else if (operatorSelected == "subtract") {
+        subtract(num1, num2)
+    } else if (operatorSelected == "multiply") {
+        multiply(num1, num2)
+    } else if (operatorSelected == "divide") {
+        divide(num1, num2)
+    } else {
+        console.log("yeah bruh it's empty, nothing has been pressed.");
+    }
+}
+
+num1 = null;
+num2 = null;
+
+operatorSelected = "empty";
+
+// think about returning values, and when / where they should be returned. 
+
+
 const display = document.querySelector(".userInput");
+display.textContent = "";
+
+const numberButton = document.querySelectorAll(".numberButton");
+numberButton.forEach(button => {
+    button.addEventListener("click", (e) => {
+    display.textContent += (button.id);
+    })
+
+})
 
 
-let temp = 123; 
-display.textContent = temp;
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", (e) => {
+    display.textContent = "";
+})
+
+const operatorButton = document.querySelectorAll(".operatorButton");
+operatorButton.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        operatorSelected = (operator.id);
+        console.log(operatorSelected);
+
+        if (num1 === null) {
+            num1 = Number(display.textContent);
+            display.textContent = "";
+        } else {
+            num2 = Number(display.textContent);
+        }
+
+    })
+})
+
+const equals = document.querySelector("#equals");
+equals.addEventListener("click", operate);
+
+/*
+think about  display number and how to refresh it if a calculation has
+already been made.
+think about num1 and num2, and how to make it possible to keep adding to a 
+number.
+*/
